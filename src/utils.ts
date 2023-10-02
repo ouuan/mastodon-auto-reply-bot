c2a9171c9a98c8efbb147aaf8064a247910d18e0
Yufan You <ouuansteve@gmail.com>
2023-10-02 16:25:53 +0800

    fix: return root for empty or "." path

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,7 @@
 /* eslint-disable import/prefer-default-export */
 
 export function access(obj: any, path: string) {
+  if (path === '.' || path === '') return obj;
   const parts = path.split('.');
   let result = obj;
   for (const part of parts) {
